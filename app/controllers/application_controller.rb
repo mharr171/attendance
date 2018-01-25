3876bae8e787fc7a2d67e19a1d3a254844e01c2a
Matthew Harris <mharr171.z@gmail.com>
2018-01-24 16:40:04 -0800

    small tweaks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ require 'faker'
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!, except: [:index]
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_location
 
